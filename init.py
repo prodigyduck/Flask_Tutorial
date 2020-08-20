909746d7dcace76070a5928fc0f684fceaf5bcfc
com24 <prodigyduck@gmail.com>
2020-08-20 09:45:41 +0900

    8.20 09시 커밋'

--- init.py
+++ init.py
@@ -1,11 +1,20 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello Flask"
+@app.route('/<int:num>')
+def inputTest(num=None):
+    return render_template('index.html', num=num)
+    
+@app.route('/calculate',methods=['POST'])
+def calculate(num=None):
+    if request.method == 'POST':
+        temp = request.form['num']
+    else:
+        temp = None
+    return redirect(url_for('inputTest',num=temp))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port='8080')
 
